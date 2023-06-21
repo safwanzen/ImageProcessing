99d07732020140d8106cfff76d6622e2d5204354
Safwan Sabin <safwan@teknomatrik.com.my>
2023-06-21 11:42:54 +0800

    threshold

--- ImageTest/ViewModels/MainViewModel.cs
+++ ImageTest/ViewModels/MainViewModel.cs
@@ -25,13 +25,61 @@ public class MainViewModel : ViewModelBase
             await stream.CopyToAsync(memstream);
             memstream.Seek(0, SeekOrigin.Begin);
             bitmap = SKBitmap.Decode(memstream);
+            var pixels = bitmap.GetPixels();
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+
+            int sy = 500;
+            int sx = 100;
+            int x = 70;
+            int y = 100;
+
+            var filter = SKColorFilter.CreateColorMatrix( new float[]
+            {
+                0.21f, 0.72f, 0.07f, 0, 0,
+                0.21f, 0.72f, 0.07f, 0, 0,
+                0.21f, 0.72f, 0.07f, 0, 0,
+                0,     0,     0,     1, 0
+            });
+
+            using var paint = new SKPaint();
+            paint.ColorFilter = filter;
+
+            var canvas = new SKCanvas(bitmap);
+            canvas.DrawBitmap(bitmap, new SKPoint(0, 0), paint); // make it grayscale
+
+            // make bitmap black and white
+            for (int row = 0; row < h; row++)
+            {
+                for (int col = 0; col < w; col++)
+                {
+                    bitmap.GetPixel(col, row).ToHsv(out var hue, out var sat, out float v);
+                    int val = v > 60f ? 100 : 0;
+                    bitmap.SetPixel(col, row, SKColor.FromHsv(hue, sat, val));
+                }
+            }
+
+            //unsafe
+            //{
+            //    uint* ptr = (uint*)pixels.ToPointer(); // BGRA8888 - 32 bits - Int32
+
+            //    // draw a 50x50 black square on the top left of the bitmap
+            //    ptr += (x * w) + y;
+
+            //    for (int row = 0; row < sy; row++)
+            //    {
+            //        for (int col = 0; col < sx; col++)
+            //        {
+            //            *ptr = 0xffff6f00;
+            //            ptr++;
+            //        }
+            //        ptr += w - sx;
+            //    }
+            //}
 
             using var data = bitmap.Encode(SKEncodedImageFormat.Png, 100);
             using var mstream = new MemoryStream(data.ToArray());
             bmp = new Bitmap(mstream);
-
-            var ptr = bitmap.GetPixels();
-
         }
         catch
         {
@@ -45,6 +93,8 @@ public class MainViewModel : ViewModelBase
     }
 
     public string Greeting => "Welcome to Avalonia!";
-    public SKBitmap bitmap {get; set; }
+    public SKBitmap bitmap { get; set; }
+    public RenderTargetBitmap renderBitmap { get; set; }
     [Reactive] public Bitmap bmp { get; set; }
+    [Reactive] public float Sliderval { get; set; } = 0;
 }
